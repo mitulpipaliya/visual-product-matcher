aabc3e2b814fbf0aab55854dad1063ea1251b8b4
mitulpipaliya <mitulpipaliya14@gmail.com>
2025-10-14 20:24:16 +0530

    fix settings.py

--- backend/matcher/settings.py
+++ backend/matcher/settings.py
@@ -12,16 +12,18 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o)998twh(orq8!0vpc*wnd(pa68d*eqhv))4hgo3oo@%uy5tol'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+if not SECRET_KEY:
+    raise ImproperlyConfigured("The SECRET_KEY environment variable must be set.")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
